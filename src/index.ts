39a627c913d40c2ec29866784a9e504677fee811
ductan2 <ndt11102003@gmail.com>
2023-09-30 16:35:15 +0700

    fix deploy

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,22 @@ import databaseServices from './services/database.services';
 import router from './routes/index.routes';
 import cookieParser from 'cookie-parser';
 import cors from "cors"
-
-
 config();
 const app = express();
 const PORT = process.env.PORT;
 app.use(cors({ origin: true, credentials: true }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/", router)
 app.use(cookieParser())
 // app.use(cors)
 databaseServices.connect();
 app.listen(PORT, () => {
   console.log(`This is http://localhost:${PORT}`)
 })
+
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   res.status(err.status).json({ error: err.message, status: err.status })
 })
-app.use('/api', router) 
+
